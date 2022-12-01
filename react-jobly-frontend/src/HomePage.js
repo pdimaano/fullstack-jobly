d0e6897c0ca8bfb1d7df31d150c44045ba94437d
Philip Dimaano <pdimaano7@gmail.com>
2022-11-30 16:05:30 -0800

    Updated styling for homepage and navigation bar

--- react-jobly-frontend/src/HomePage.js
+++ react-jobly-frontend/src/HomePage.js
@@ -15,7 +15,10 @@ function HomePage() {
 
   return (
     <div className="HomePage">
-      <h1>Jobly</h1>
+      <div>
+        <h1>Jobly</h1>
+        <p>Find your dream job on a Korean beach!</p>
+      </div>
     </div>
    );
 }

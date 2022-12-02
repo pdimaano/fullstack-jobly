5a63b04322933c751e288e74d9450add74f5d204
Spencer Taira <spencermtaira@gmail.com>
2022-12-02 13:47:51 -0800

    added buttons to homepage

--- react-jobly-frontend/src/HomePage.js
+++ react-jobly-frontend/src/HomePage.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import "./HomePage.css";
 import userInfoContext from "./userInfoContext";
+import {useNavigate} from "react-router-dom";
 
 /**
  *  Renders home page
@@ -16,14 +17,31 @@ function HomePage() {
   const userInfo = useContext(userInfoContext);
   console.debug("HomePage");
 
-//TODO: add signup/login buttons
+  const navigate = useNavigate();
+
+  function signupClick(e) {
+    e.preventDefault();
+    navigate('/signup');
+  }
+
+  function loginClick(e) {
+    e.preventDefault();
+    navigate('/login');
+  }
+
 
   return (
     <div className="HomePage">
       <div>
         <h1>Jobly</h1>
-        {userInfo.user.firstName && <p>Welcome {userInfo.user.firstName}! </p>}
+        {userInfo.loggedIn && <p>Welcome {userInfo.user.firstName}! </p>}
         <p>Find your dream job on a Korean beach!</p>
+        {!userInfo.loggedIn &&
+        <div className="HomePage-buttons">
+          <button onClick={signupClick} className="btn btn-primary">Sign Up</button>
+          <button onClick={loginClick} className="btn btn-primary">Log In</button>
+        </div>
+        }
       </div>
     </div>
   );
